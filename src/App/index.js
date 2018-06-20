0d0d75e97d16e8c64fd72f43558e95a09a0a7392
Jes Ababon <jesababon@gmail.com>
2018-06-20 12:12:03 -0400

    added some details to homepage

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import "./style.css";
+import Homepage from '../HomePage';
 
 class App extends Component {
   render() {
-    return <div className="App">Groupi3</div>;
+  return (
+  <div className="App">
+  <Homepage />
+  </div>);
   }
 }
 
---
+++ src/HomePage/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class HomePage extends Component {
+  render() {
+  return (
+  <div className="container">
+  <header>Groupi3</header>
+  <nav>Username Password Register LogIn</nav>
+  <div className="landingBody">
+  <p>Genre Location Search</p>
+  </div>
+  <footer>Copyright Group3</footer>
+  </div>
+);
+  }
+}
+
+export default HomePage;
