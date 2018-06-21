e9c0a637324669ee18a865dbf70de546d6852e84
Jes Ababon <jesababon@gmail.com>
2018-06-21 11:25:18 -0400

    added events and artists tables to schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
 -- DROP DATABASE groupi3db;
 -- CREATE DATABASE groupi3db;
 
 -- \c groupi3db
 
-=======
-
--- DROP DATABASE groupi3db;
--- CREATE DATABASE groupi3db;
-
--- \c groupi3db
-
->>>>>>> master
 CREATE TABLE users (
     id SERIAL PRIMARY KEY NOT NULL,
     username VARCHAR(20),
@@ -23,3 +14,19 @@ CREATE TABLE comments (
     user_id INT REFERENCES users(id),
     content TEXT
 );
+
+CREATE TABLE events (
+    id SERIAL PRIMARY KEY NOT NULL,
+    artist_id VARCHAR(255) REFERENCES artists(id),
+    date DATE,
+    artist_name VARCHAR(255),
+    venue VARCHAR(255),
+    address VARCHAR(255),
+    ticket_link VARCHAR(255)
+);
+
+CREATE TABLE artists (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255),
+    genre VARCHAR(255)
+)
