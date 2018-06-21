aca91d4f516b9f531359fe3ffb3ca67ffa724e9d
Dan <danhemerlein@gmail.com>
2018-06-21 12:02:11 -0400

    made sure seed worked

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,7 @@
--- DROP DATABASE groupi3db;
--- CREATE DATABASE groupi3db;
+DROP DATABASE groupi3db;
+CREATE DATABASE groupi3db;
 
--- \c groupi3db
+\c groupi3db
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY NOT NULL,
@@ -15,18 +15,20 @@ CREATE TABLE comments (
     content TEXT
 );
 
+CREATE TABLE artists
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255),
+    genre VARCHAR(255)
+);
+
 CREATE TABLE events (
     id SERIAL PRIMARY KEY NOT NULL,
-    artist_id VARCHAR(255) REFERENCES artists(id),
+    artist_id INT REFERENCES artists(id),
     date VARCHAR (15),
     artist_name VARCHAR(255),
     venue VARCHAR(255),
     address VARCHAR(255),
-    ticket_link VARCHAR(255)
+    ticket_link VARCHAR(500)
 );
 
-CREATE TABLE artists (
-    id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(255),
-    genre VARCHAR(255)
-)
