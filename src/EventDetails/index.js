631975dc908f3f1fa3938c96c752351b1f710a56
Joel Stewart <iamjoelkstewart@gmail.com>
2018-06-26 09:19:16 -0400

    added home button and scroll on details

--- src/EventDetails/index.js
+++ src/EventDetails/index.js
@@ -3,6 +3,11 @@ import "./style.css";
 class EventDetails extends Component {
   render() {
     return (
+      <div className="buttons">
+        <form action="/" method="get">
+        <button type="submit">HOME</button>
+        </form>
+        <div className="detailsbox">
       <div className="EventDetails" key={"oneEvent"}>
           <h4>{this.props.title}</h4>
           <p className="eventID"><strong>Event ID:</strong>{this.props.event_id}</p>
@@ -12,6 +17,8 @@ class EventDetails extends Component {
           <p><a href={this.props.facebook_url} target="_blank">RSVP on Facebook</a></p>
           <p><span>Tickets are {this.props.ticket_status} </span><a href={this.props.ticket_link} target="_blank">Get Tickets </a></p>
           <p><a href={'event/'+this.props.event_id}>View Details</a></p>
+      </div>
+      </div>
       </div>
     );
   }
