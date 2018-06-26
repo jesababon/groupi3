d84486e1bdde123cfd7dab5566c35f0f6791d42f
Dan <danhemerlein@gmail.com>
2018-06-26 10:19:21 -0400

    added event page

---
+++ src/EventPage/index.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import "./style.css";
+import EventDetails from '../EventDetails';
+
+class EventPage extends Component {
+
+     constructor(props){
+          super(props)
+
+          this.state = {
+             events: []
+          }
+      }
+  
+  componentDidMount () {
+      console.log('component mounted');
+      fetch('/api-events.json')
+      .then(response => response.json())
+      .then(events => {
+          this.setState({
+              events: events
+          },()=> {
+            //   console.log('logging state', this.state.events);
+          });
+      });
+  }
+
+  componentDidUpdate () {
+      console.log('updated'); 
+  }
+
+ render(){
+       return(   
+        <div>
+           {
+            //    this.state.events.length && 
+            (
+                   <div className="EventPage">
+                       {this.state.events.map((event, index) => {
+                           return (
+                               <div className="showEvents" key={index}>
+                                   <EventDetails
+                                       id={index}
+                                       event_id={event.id}
+                                       title={event.title}
+                                       date={event.formatted_datetime}
+                                       venue={event.venue.name}
+                                       location={event.formatted_location}
+                                       facebook_url={event.facebook_rsvp_url}
+                                       ticket_status={event.ticket_status}
+                                       ticket_link={event.ticket_url}
+                                   />
+                               </div>
+                                    )
+                                }
+                            )
+                        }
+                     </div>
+                    )
+                }
+
+               {
+                //    !this.state.events.length && 
+                (
+                       <div className="EventPage">
+                        <h1>There are no events for that artist</h1>
+                   </div>
+               )
+                }
+        </div>
+       );
+    };
+}
+
+export default EventPage;
